34536f2bef92527fccae1deb87e293a810b72c28
Aaron Sharon Dsouza <aasds@eg.dk>
2024-11-29 10:10:00 +0530

    Feat : Added the new Fields and Proper working of the API

--- Models/Vechile.cs
+++ Models/Vechile.cs
@@ -9,5 +9,15 @@ namespace RTO.Models
         public string Model { get; set; }
         public string Owner { get; set; }
         public DateTime RegistrationDate { get; set; }
+
+        // New fields for personal information
+        public string VehicleName { get; set; }
+        public string OwnerAddress { get; set; }
+        public string OwnerContactNumber { get; set; }
+        public string OwnerEmail { get; set; }
+
+        // New field for price
+        public float Price { get; set; }
+
     }
 }
